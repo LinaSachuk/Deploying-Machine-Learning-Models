08d95ca7f73a4e9559127dce9ccb583112e914e0
Lina Sachuk <sachuklina@gmail.com>
2020-06-13 17:01:50 -0600

    added test 2

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
+import pandas as pd
+import numpy as np
+import altair as alt
 import streamlit as st
 
-st.title('Hello World')
-st.write('Pick an option')
+df = pd.DataFrame(
+    np.random.randn(200, 3),
+    columns=['a', 'b', 'c'])
 
-keys = ['Normal', 'Uniform']
-dist_key = st.selectbox('Which Distribution do you want?', keys)
+st.text('This is some text.')
 
-st.write('You have chosen {}'.format(dist_key))
+c = alt.Chart(df).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
+st.write(c)
