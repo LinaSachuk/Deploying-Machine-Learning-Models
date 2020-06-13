f6d1c42b8b2630346d46f06a2e3724b696206ead
Lina Sachuk <sachuklina@gmail.com>
2020-06-13 17:21:26 -0600

    added test 3

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from PIL import Image
 import pandas as pd
 import numpy as np
 import altair as alt
@@ -7,9 +8,37 @@ df = pd.DataFrame(
     np.random.randn(200, 3),
     columns=['a', 'b', 'c'])
 
+st.title('This is a title')
 st.text('This is some text.')
+st.write('Hello, *World!* :sunglasses:')
+
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40],
+}))
 
 c = alt.Chart(df).mark_circle().encode(
     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 
 st.write(c)
+
+code = '''def hello():
+            print("Hello, Streamlit!")'''
+st.code(code, language='python')
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.area_chart(chart_data)
+
+image = Image.open(
+    "sunrise.jpg")
+
+st.image(image, caption='Sunrise by the mountains',
+         use_column_width=True)
