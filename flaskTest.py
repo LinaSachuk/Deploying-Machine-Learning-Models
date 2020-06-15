dea08aae0ada94c077672e82678cb31556ae993d
Lina Sachuk <sachuklina@gmail.com>
2020-06-14 22:39:13 -0600

    added get requestk

--- flaskTest.py
+++ flaskTest.py
@@ -1,5 +1,5 @@
 # ====================================== Flask testing =========================
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,21 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Welcome to Home"
+
+
+@app.route('/add')
+def add():
+    a = 10
+    b = 90
+    return str(a + b)
+
+
+@app.route("/addParam", methods=['GET'])
+def add_param():
+    a = request.args.get('a')
+    b = request.args.get('b')
+
+    return str(int(a) + int(b))
 
 
 if __name__ == '__main__':
