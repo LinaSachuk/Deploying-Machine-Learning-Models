d5908e1fa08164d8d9dd08cf0fb8f53e0d7027db
Lina Sachuk <sachuklina@gmail.com>
2020-06-22 23:06:59 -0600

    added currency format

--- streamlit_basics/basics.py
+++ streamlit_basics/basics.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import  numpy as np
-
+import numpy as np
 import pandas as pd
 
 
@@ -8,47 +7,44 @@ st.title("Sample App")
 
 st.text("Sample text for the app")
 
-#
-# st.dataframe(pd.DataFrame({
-#     '1st_Col':[1,2,4,5,6]
-#     ,'2nd_col':[352,6,32,6,2]
-# }))
-#
-#
-# chart_data = pd.DataFrame(
-#     np.random.randn(20,3),
-#     columns=['1st','2nd','3rd']
-# )
-#
-# st.line_chart(chart_data)
-#
-#
-# map_data = pd.DataFrame(
-#     np.random.randn(1000,2) / [50,50] + [37.76,-122.3],
-#     columns=['lat','lon']
-#
-# )
-#
-# st.map(map_data)
+
+st.dataframe(pd.DataFrame({
+    '1st_Col': [1, 2, 4, 5, 6], '2nd_col': [352, 6, 32, 6, 2]
+}))
+
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['1st', '2nd', '3rd']
+)
+
+st.line_chart(chart_data)
+
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.3],
+    columns=['lat', 'lon']
+
+)
+
+st.map(map_data)
 
 if st.checkbox('Show Details'):
     st.dataframe(pd.DataFrame({
-         '1st_Col':[1,2,4,5,6]
-         ,'2nd_col':[352,6,32,6,2]
-     }))
+        '1st_Col': [1, 2, 4, 5, 6],
+        '2nd_col': [352, 6, 32, 6, 2]
+    }))
 
 
-df = pd.DataFrame(np.random.randn(20,3),columns=["a","b","c"])
+df = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
-col = st.selectbox('Select the Columns to be Displayed',df.columns)
+col = st.selectbox('Select the Columns to be Displayed', df.columns)
 
 st.line_chart(df[col])
 
 
+df = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
-df = pd.DataFrame(np.random.randn(20,3),columns=["a","b","c"])
-
-cols = st.multiselect('Select the Columns to be Displayed',df.columns)
+cols = st.multiselect('Select the Columns to be Displayed', df.columns)
 
 st.line_chart(df[cols])
-
--- streamlit_basics/get_house_price.py
+++ streamlit_basics/get_house_price.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import  LinearRegression
-from  sklearn.model_selection import  train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 import time
 
@@ -14,28 +14,32 @@ st.title('Predict The House Price')
 show_data = st.checkbox('See the House Data')
 
 if show_data:
-    st.write(data.head())
+    st.write(data.head(10))
 
 
-sqft = st.slider("Square Feet of house Required :",int(data.sqft_living.min()),int(data.sqft_living.max()),int(data.sqft_living.mean()))
+sqft = st.slider("Square Feet of house Required :", int(
+    data.sqft_living.min()), int(data.sqft_living.max()), int(data.sqft_living.mean()))
 
-bath = st.slider("No. of Bathrooms :",int(data.bathrooms.min()),int(data.bathrooms.max()),int(data.bathrooms.mean()))
-bed = st.slider("No. of Bedrooms :",int(data.bedrooms.min()),int(data.bedrooms.max()),int(data.bedrooms.mean()))
-floor = st.slider("No. of Floors:",int(data.floors.min()),int(data.floors.max()),int(data.floors.mean()))
+bath = st.slider("No. of Bathrooms :", int(data.bathrooms.min()), int(
+    data.bathrooms.max()), int(data.bathrooms.mean()))
+bed = st.slider("No. of Bedrooms :", int(data.bedrooms.min()),
+                int(data.bedrooms.max()), int(data.bedrooms.mean()))
+floor = st.slider("No. of Floors:", int(data.floors.min()),
+                  int(data.floors.max()), int(data.floors.mean()))
 
 
-X = data.drop('price',axis=1)
+X = data.drop('price', axis=1)
 y = data.price
 
 model = LinearRegression()
 
-model.fit(X,y)
+model.fit(X, y)
 
-pred = model.predict([[sqft,bath,bed,floor]])[0]
-
-
-
+pred = model.predict([[sqft, bath, bed, floor]])[0]
 
 
 if st.button("Get House Price"):
-    st.header("Estimated House Price is USD {}".format(int(pred)))
+
+    formatted_float = "${:,.2f}".format(int(pred))
+    print(formatted_float)
+    st.header("Estimated House Price is USD {}".format(formatted_float))
